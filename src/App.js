0b2e6b080169800eb584df3353bf796c339b9109
SharukSingh <sharuksingh@outlook.com>
2019-08-12 09:24:21 +0545

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import result from "./test-data"
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// http://itunes.apple.com/us/lookup?id=1389971325
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: result
+      }
+  }
+
+  componentDidMount() {
+    // console.log(result)
+  }
+
+
+  getOption() {
+    let title = this.state.data.results.map(item => item.trackName)
+    // console.log(title)
+    let randomNumber = Math.floor(Math.random() * title.length)
+    console.log(randomNumber)
+
+    let tempOption = title[randomNumber]
+
+    for(let i = 0 ; i<title.length; i++) {
+      if( title[i] === tempOption) {
+        title.splice(i,1)
+      }
+    }
+    console.log(title)
+
+    return tempOption
+  }  
+
+  render() {
+
+  
+    return(
+      <div className="App">
+        <div className="App-header">
+          <h1>Guess That Song</h1>
+
+          <img src={this.state.data.results[0].artworkUrl100} alt="" />
+          <br />
+          
+          <audio src={this.state.data.results[0].previewUrl} controls>
+            Audio not supported
+          </audio>
+          <br />
+          
+          <div>
+            <button>{this.getOption()}</button>
+            <button>{this.getOption()}</button>
+            <button>{this.getOption()}</button>
+            <button>{this.getOption()}</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
